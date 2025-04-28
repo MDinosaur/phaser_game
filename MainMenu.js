a12b983f597f1e1522f707d681809b7ed4625767
MDinosaur <onlyforboogie@icloud.com>
2025-04-28 12:06:51 -0400

    Update MainMenu.js

--- MainMenu.js
+++ MainMenu.js
@@ -12,30 +12,14 @@ export default class MainMenu extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, 'TitleBG');
-        this.add.image(192, 69, 'DinoFishing');
-        
-        const playButton = this.add.image(192, 40, 'Play').setInteractive();
-
-        this.tweens.add({
-            targets: playButton, 
-            y: 320, 
-            duration: 1000,
-            yoyo: true, 
-            repeat: -1, 
-            ease: 'Sine.easeInOut' 
-        });
-        
-        playButton.on('pointerdown', () => {
-            console.log('Play button clicked!');
-        });
         
     }
 }
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 1600,
     scene: [MainMenu],
 };
  const game = new Phaser.Game(config);
