af2c26076b9d911e6a42afd4a6f3ce38b5af5362
MDinosaur <onlyforboogie@icloud.com>
2025-04-29 16:51:32 -0400

    Update MainMenu.js

--- MainMenu.js
+++ MainMenu.js
@@ -27,6 +27,9 @@ export default class MainMenu extends Phaser.Scene {
         
         
         const playButton = this.add.image(720, 365, 'Play').setScale(0.6).setInteractive({
+            hitArea: new Phaser.Geom.Rectangle(450, 715, 450, 180),
+     hitAreaCallback: Phaser.Geom.Rectangle.Contains,
+     useHandCursor: true
         });
         
         this.tweens.add({
@@ -43,6 +46,9 @@ playButton.on('pointerdown', (pointer) => {
         });
 
         const optionButton = this.add.image(720, 365, 'Option').setScale(0.6).setInteractive({
+            hitArea: new Phaser.Geom.Rectangle(850, 1115, 450, 180),
+     hitAreaCallback: Phaser.Geom.Rectangle.Contains,
+     useHandCursor: true
         });
 
         this.tweens.add({
