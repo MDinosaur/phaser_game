b433e162948f8027524983638d58d6b09b7c193b
MDinosaur <onlyforboogie@icloud.com>
2025-04-30 00:29:47 -0400

    Update Cabin.js

--- Cabin.js
+++ Cabin.js
@@ -5,14 +5,21 @@ export default class Cabin extends Phaser.Scene {
     }
 
    preload (){
-//this.load.image('CabinBG', 'assets'); this.load.image('Note', 'assets'); this.load.image('UI', 'assets'); this.load.image('Inventory', 'assets'); this.load.image('Journal', 'assets');
+
+       this.load.image('CabinBG', 'Dino Fishing Cabin/Cabin.png');
+       this.load.image('Note', 'Dino Fishing Cabin/Note.png');
+// this.load.image('UI', 'assets'); this.load.image('Inventory', 'assets'); this.load.image('Journal', 'assets');
      
    }
 
    create(){
- //const CabinBG = this.add.image(720, 400, 'CabineBG').setScale(0.65); console.log(`TitleBG: x=${TitleBG.x}, y=${TitleBG.y}`); const placeholder = this.add.image(-,0 'placeholder').setScale(0.6);
+
+       const CabinBG = this.add.image(720, 400, 'CabinBG').setScale(0.65); 
+           console.log(`CabinBG: x=${CabinBG.x}, y=${CabinBG.y}`);
+ // const placeholder = this.add.image(-,0 'placeholder').setScale(0.6);
     }
 
    update (){
 //probably have constant updates to check if the player has been through the prolouge (taking the note offf the wall)
    }
+}
