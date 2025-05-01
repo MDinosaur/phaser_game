bd9c8598029306a3f7180da015bd349eb75a99ab
MDinosaur <onlyforboogie@icloud.com>
2025-05-01 18:53:11 -0400

    Update Cabin.js

--- Cabin.js
+++ Cabin.js
@@ -14,7 +14,7 @@ export default class Cabin extends Phaser.Scene {
        this.load.image('JournalOpened', 'Dino Fishing Cabin/Journal_opened.png');
        this.load.image('JournalIcon', 'Dino Fishing Cabin/Journalicon.png');
        this.load.image('InventoryIcon', 'Dino Fishing Cabin/Inventoryicon.png');
-       this.load.image('NormalEel', 'Dino Fishing Cabin/Normal Eel Spirite.png')
+       this.load.image('NormalEel', 'Dino Fishing Cabin/Normal Eel Spirite.png');
 // this.load.image('UI', 'assets'); this.load.image('Inventory', 'assets'); this.load.image('Journal', 'assets');
        //this.load.text('Alien-Dictionary', 'assets'); the startin global set up for the dictonary
      
@@ -76,8 +76,8 @@ export default class Cabin extends Phaser.Scene {
        const UI_Closed = this.add.image(720, 400, 'UI_Closed').setScale(0.62);
            console.log(`UI_Closed: x=${UI_Closed.x}, y=${UI_Closed.y}`);
        //const Inventory = this.add.image(720, 400, 'Inventory').setScale(0.65); const Journal = this.add.iamge(720, 400, 'Journal').setScale(0.65);
-       const NormalEel = this.add.image(490, 550, 'NormalEel').setScale(1);
-           console.log(`NormalEel: x=${NormalEel.x}, y=${NormalEel.y}`);
+       //const NormalEel = this.add.image(490, 550, 'NormalEel').setScale(1);
+           //console.log(`NormalEel: x=${NormalEel.x}, y=${NormalEel.y}`);
        //if status = Hungry change.texture('HungryEel') and so on...
  // const placeholder = this.add.image(-,0 'placeholder').setScale(0.6);
        const JournalIcon = this.add.image(1128, 520, 'JournalIcon').setScale(0.61).setInteractive({
@@ -120,7 +120,7 @@ export default class Cabin extends Phaser.Scene {
     //Translation check to see if soemthing can be translated
         //checkTranslation(word, dictionaryLines) {
             //let Transaltion = null;
-            //dictionaryLines.forEach((line) +> {
+            //dictionaryLines.forEach((line) => {
                 //let parts = line.split(':');
                 //if (part.length === 2 ) {
                     //let key = parts[0].trim();
