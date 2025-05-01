902fa3ea9a879cf72ba7cc87b98999daa585c1ae
MDinosaur <onlyforboogie@icloud.com>
2025-05-01 14:33:38 -0400

    Update Cabin.js

--- Cabin.js
+++ Cabin.js
@@ -16,11 +16,34 @@ export default class Cabin extends Phaser.Scene {
        this.load.image('InventoryIcon', 'Dino Fishing Cabin/Inventoryicon.png');
        this.load.image('NormalEel', 'Dino Fishing Cabin/Normal Eel Spirite.png')
 // this.load.image('UI', 'assets'); this.load.image('Inventory', 'assets'); this.load.image('Journal', 'assets');
+       //this.load.text('Alien-Dictionary', 'assets'); the startin global set up for the dictonary
      
    }
 
    create(){
 
+//Translation set-up
+       //const WordsToTranslate = []
+       //const DictionaryText = this.cache.text.get('Alien-Dictionary')
+       //const lines = DictionaryText.split('\n');
+       //let Translations = {}
+       //WordsToTranslate.forEach((word) => {
+           //let Translation = this.checkTranslation(word, lines);
+               //if (Translation) {
+                   //translations[words] = Translation 
+                   //alert(word +'Translation:'+ Translation);
+               //} else {
+                   //let userTranslation = prompt(`Enter translation for '${word}' (alien language):`);
+                   //if (userTranslation) {
+                       //translations[words] = userTranslation;
+                       //alert(`You translated '${word}' to: ${userTranslation}`);
+               // } else {
+                   //alert(`No translation entered for '${word}'.`);} 
+       //}
+    //});
+
+       //localStorage.setItem('Translations', JSON.stringify(translations));
+       
        const CabinBG = this.add.image(720, 400, 'CabinBG').setScale(0.65); 
            console.log(`CabinBG: x=${CabinBG.x}, y=${CabinBG.y}`);
        const Note = this.add.image(570, 295, 'Note').setScale(0.6).setInteractive({
@@ -39,7 +62,7 @@ export default class Cabin extends Phaser.Scene {
        const UI_Closed = this.add.image(720, 400, 'UI_Closed').setScale(0.62);
            console.log(`UI_Closed: x=${UI_Closed.x}, y=${UI_Closed.y}`);
        //const Inventory = this.add.image(720, 400, 'Inventory').setScale(0.65); const Journal = this.add.iamge(720, 400, 'Journal').setScale(0.65);
-       const NormalEel = this.add.image(720, 500, 'NormalEel').setScale(0.6);
+       const NormalEel = this.add.image(420, 700, 'NormalEel').setScale(0.8);
            console.log(`NormalEel: x=${NormalEel.x}, y=${NormalEel.y}`);
        //if status = Hungry change.texture('HungryEel') and so on...
  // const placeholder = this.add.image(-,0 'placeholder').setScale(0.6);
@@ -65,6 +88,23 @@ export default class Cabin extends Phaser.Scene {
     console.log('Inventory button clicked at: ', pointer.x, pointer.y); 
        });
     }
+
+    //Translation check to see if soemthing can be translated
+        //checkTranslation(word, dictionaryLines) {
+            //let Transaltion = null;
+            //dictionaryLines.forEach((line) +> {
+                //let parts = line.split(':');
+                //if (part.length === 2 ) {
+                    //let key = parts[0].trim();
+                    //let value = parts[1].trim();
+                //if (key === word) {
+                    //translation = value; }
+            //}
+    //}); return translation; }
+    
+    
+                   
+    
 
    update (){
 //probably have constant updates to check if the player has been through the prolouge (taking the note offf the wall)
